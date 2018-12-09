84ee3b9fdc378dbdb50ced9bf7def0725b7de837
Gabriella Watkins <gabbleswat@gmail.com>
2018-12-09 13:08:16 +0000

    Corrected interface bugs on new problem form

--- app/src/main/java/com/backscratch/gabriella/aeonfire/survey/NewProblemActivity.java
+++ app/src/main/java/com/backscratch/gabriella/aeonfire/survey/NewProblemActivity.java
@@ -1,9 +1,14 @@
 package com.backscratch.gabriella.aeonfire.survey;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.backscratch.gabriella.aeonfire.LoginActivity;
 import com.backscratch.gabriella.aeonfire.R;
+import com.backscratch.gabriella.aeonfire.ReportsOverviewActivity;
 
 public class NewProblemActivity extends AppCompatActivity {
 
@@ -11,5 +16,17 @@ public class NewProblemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_problem);
+
+        Button saveButton = (Button) findViewById(R.id.btnSAVE);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //attemptLogin();
+
+                Intent save = new Intent(NewProblemActivity.this, NewSurveyActivity.class);
+                startActivity(save);
+
+            }
+        });
     }
 }
